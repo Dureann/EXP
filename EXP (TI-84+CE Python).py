3a9e80bd90fab8b5df442ddc387ea261f3ccfe33
tr77 <44878281+TryMaker77@users.noreply.github.com>
2022-02-28 20:14:14 -0600

    Update EXP (TI-84+CE Python).py

--- EXP (TI-84+CE Python).py
+++ EXP (TI-84+CE Python).py
@@ -5,24 +5,23 @@ end = "y"
 #main code
 print("""
 Welcome To
- __________   ___ .______   
-|   ____\  \ /  / |   _  \  
-|  |__   \  V  /  |  |_)  | 
-|   __|   >   <   |   ___/  
-|  |____ /  .  \  |  |      
-|_______/__/ \__\ | _|      
- EXP for TI84+CE Python v1.0.7; Beta
+ __________   ___ ______   
+|   ____\  \ /  /|   _   \  
+|  |__   \  V  / |  |_)  | 
+|   __|   >   <  |   ____/  
+|  |____ /  _  \ |  |      
+|_______/__/ \__\|__|      
+ EXPv1.1.1
  Durian Technologies 2022
- By TR77
- """)
-while (end != "z"):
-    end = val_a = input("Your Value for a:")
-    end = val_x = input("Your expresion value (p/n)")
-    end = val_b = input("Your Value for b:")
-    end = val_t = input("Your Value for x:")
+ By TR77""")
+while (1 == 1):
+    val_a = input("Your Value for a: ")
+    val_x = input("Your expresion value (p/n) ")
+    val_b = input("Your Value for b: ")
+    val_t = input("Your Value for x: ")
     if val_x == "p":
         val_y = (float(val_a) * (1 + float(val_b)) ** float(val_t))
         print("Your valye for y is " + str(val_y))
     if val_x == "n":
         val_y = (float(val_a) * (1 - float(val_b)) ** float(val_t))
-        print("Your valye for y is " + str(val_y))
+        print("Your value for y is \n" + str(val_y))
