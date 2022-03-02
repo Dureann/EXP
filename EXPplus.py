360db463434a19506206d1fd3deb2bf0e9d9a343
tr77 <44878281+TryMaker77@users.noreply.github.com>
2022-03-01 22:09:57 -0600

    Debugged+Reowrked

--- EXPplus.py
+++ EXPplus.py
@@ -1,4 +1,28 @@
 #EXPplus
+#Imports
+import time
+    #Functions
+#Linear Functions
+def line():
+    print("Div Line:------")
+    val_m2 = input("Your Value for a: ")
+    val_x2 = input("Your Value for x: ")
+    val_b2 = input("Your Value for b: ")
+    val_y2 = ((float(val_m2) * float(val_x2)) + float(val_b2))
+    print("Your valye for y is " + str(val_y2))
+#Exponential Fucntions
+def expon():
+    print("Div Line:------")
+    val_a = input("Your Value for a: ")
+    val_x = input("Your expresion value (+/-) ")
+    val_b = input("Your Value for b: ")
+    val_t = input("Your Value for x: ")
+    if val_x == "+":
+        val_y = (float(val_a) * (1 + float(val_b)) ** float(val_t))
+        print("Your valye for y is " + str(val_y))
+    if val_x == "-":
+        val_y = (float(val_a) * (1 - float(val_b)) ** float(val_t))
+        print("Your value for y is" + str(val_y))
 #Intro
 print("""
 Welcome To
@@ -8,37 +32,22 @@ Welcome To
 |   __|   >   <  |   ____/|_   _|
 |  |____ /  _  \ |  |       |_|  
 |_______/__/ \__\| _|             
- EXPplusv1.0.9
+ EXPplusv1.1.6
  Durian Technologies 2022
  By TR77""")
+time.sleep(1.5)
 #While
 while 1 == 1:
 #Function Type
-    ftyp = input("""Function type
-    "e" = Exponential Function
-    "l" = Linear Function""")
+    ftyp = input("""Div Line:------
+    Function type
+    "q" = Quit Program
+    "e" = Exponential Function (y=a(b)^x)
+    "l" = Linear Function (y=mx+b)
+    Input: """)
+    if ftyp == "q":
+        quit()
     if ftyp == "e":
         expon()
     if ftyp == "l":
         line()
-#Linear Functions
-        def line():
-            print("New Equasion:------")
-            al_m2 = input("Your Value for a: ")
-            val_x2 = input("Your expresion value (p/n) ")
-            val_b2 = input("Your Value for b: ")
-            val_y2 = ((float(val_m2) * float(val_x2)) + float(val_b2))
-            print("Your valye for y is " + str(val_y2))
-#Exponential Fucntions
-        def expon():
-            print("New Equasion:------")
-            val_a = input("Your Value for a: ")
-            val_x = input("Your expresion value (p/n) ")
-            val_b = input("Your Value for b: ")
-            al_t = input("Your Value for x: ")
-            if val_x == "p":
-                val_y = (float(val_a) * (1 + float(val_b)) ** float(val_t))
-                print("Your valye for y is " + str(val_y))
-            if val_x == "n":
-                val_y = (float(val_a) * (1 - float(val_b)) ** float(val_t))
-                print("Your value for y is" + str(val_y))
